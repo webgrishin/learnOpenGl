8c537a21940aa5d62a17b1c15da206e3e6ea42a0
Konstantin <webgrishin@gmail.com>
2020-10-07 00:07:19 +0300

    offsetX triangle in shader.vs

--- src/main.cpp
+++ src/main.cpp
@@ -188,6 +188,7 @@ int main(void)
         //draw first triangle
 //        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         ourShader.use();
+        ourShader.setFloat("xOffset", 0.5f);
 //        glBindVertexArray(VAOs[0]); // поскольку у нас есть только один VАО, то нет необходимости связывать его каждый раз (но мы сделаем это, чтобы всё было немного организованнее)
         glBindVertexArray(VAO); // поскольку у нас есть только один VАО, то нет необходимости связывать его каждый раз (но мы сделаем это, чтобы всё было немного организованнее)
         glDrawArrays(GL_TRIANGLES, 0, 3);

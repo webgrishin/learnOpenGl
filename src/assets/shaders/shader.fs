97545942d8b8ff48138a1f1991e0bcdde1d44f3b
Konstantin <webgrishin@gmail.com>
2020-10-13 00:04:27 +0300

    textures

--- src/assets/shaders/shader.fs
+++ src/assets/shaders/shader.fs
@@ -1,8 +1,17 @@
 #version 460 core
 out vec4 FragColor;
+
 in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
-};
+	//FragColor = mix(texture(texture1, vec2(-TexCoord.x, TexCoord.y)),texture(texture2, TexCoord), 0.8) ;1
+	//FragColor = texture(texture1, TexCoord)* vec4(ourColor, 1.0);
+    //FragColor = texture(texture1, vec2(-TexCoord.x, TexCoord.y));
+	FragColor = texture(texture1, TexCoord);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "Renderer/ShaderProgram.h"
+#include "stb_image.h"
 
 /*
  * base example code for setting up-window and opengl context taken from site https://www.glfw.org/documentation.html
@@ -65,15 +66,21 @@ int main(void)
         return -1;
 
     float vertices[] = {
-            // координаты         // цвета
-            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // нижняя правая вершина
-            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // нижняя левая вершина
-            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // верхняя вершина
+            // координаты          // цвета           // текстурные координаты
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // верхняя правая вершина
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // нижняя правая вершина
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // нижняя левая вершина
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // верхняя левая вершина
+    };
+    unsigned int indices[] = {
+            0, 1, 3, // первый треугольник
+            1, 2, 3  // второй треугольник
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // сначала связываем объект вершинного массива, затем связываем и устанавливаем вершинный буфер(ы), и затем конфигурируем вершинный атрибут(ы).
     glBindVertexArray(VAO);
 
@@ -81,12 +88,65 @@ int main(void)
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // координатные артибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // цветовые атрибуты
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // текстурные атрибуты
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // загрузка и создание текстуры
+    // -------------------------
+    stbi_set_flip_vertically_on_load(true);
+    unsigned int textures[2];
+    glGenTextures(2, textures);
+
+    glBindTexture(GL_TEXTURE_2D, textures[0]); // все последующие GL_TEXTURE_2D-операции теперь будут влиять на данный текстурный объект
+    // установка параметров наложения текстуры
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // установка параметров фильтрации текстуры
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // загрузка изображения, создание текстуры и генерирование mipmap-уровней
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\awesomeface.png", &width, &height, &nrChannels, 0);
+//    unsigned char* data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\flowers_texture734.jpg", &width, &height, &nrChannels, 0);
+//    unsigned char* data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\wooden_container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    glBindTexture(GL_TEXTURE_2D, textures[1]); // все последующие GL_TEXTURE_2D-операции теперь будут влиять на данный текстурный объект
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\wooden_container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -95,9 +155,15 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Связывание текстуры
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
         ourShader.use();
         glBindVertexArray(VAO); // поскольку у нас есть только один VАО, то нет необходимости связывать его каждый раз (но мы сделаем это, чтобы всё было немного организованнее)
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -107,6 +173,8 @@ int main(void)
     // Опционально: освобождаем все ресурсы, как только они выполнили своё предназначение
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteTextures(2, textures);
 
     glfwTerminate();
     return 0;

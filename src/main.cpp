c442c4fbab6da6d5ef1e9f4c13db25f19a9048c5
Konstantin <webgrishin@gmail.com>
2020-10-20 00:43:40 +0300

    coordinate system, perspective

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ const unsigned int WINDOW_HEIGHT = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window, RenderEngine::ShaderProgram &ourShader, float &ratio);
+void processInput(GLFWwindow* window);
 void changeRatioVisibleImage(RenderEngine::ShaderProgram &ourShader, float &ratio, float step);
 
 int main(void)
@@ -70,90 +71,56 @@ int main(void)
  * and "SimpleCoding" https://www.youtube.com/playlist?list=PL6x9Hnsyqn2XU7vc8-oFLojbibK91fVd-*/
 // Компилирование нашей шейдерной программы
 
-    RenderEngine::ShaderProgram ourShader("C:\\learnOpenGl-CLion\\src\\assets\\shaders\\shader.vs", "C:\\learnOpenGl-CLion\\src\\assets\\shaders\\shader.fs");
+    RenderEngine::ShaderProgram ourShader("C:\\learnOpenGl-VS\\src\\assets\\shaders\\shader.vs", "C:\\learnOpenGl-VS\\src\\assets\\shaders\\shader.fs");
     if (!ourShader.isCompiled())
         return -1;
 
     float vertices[] = {
-            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // верхняя правая вершина
-            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // нижняя правая вершина
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // нижняя левая вершина
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // верхняя левая вершина
+        // координаты       // текстурные координаты
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // верхняя правая вершина
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // нижняя правая вершина
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // нижняя левая вершина
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // верхняя левая вершина 
     };
     unsigned int indices[] = {
-            0, 1, 3, // первый треугольник
-            1, 2, 3  // второй треугольник
+        0, 1, 3, // первый треугольник
+        1, 2, 3  // второй треугольник
     };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    unsigned int VBO[2], VAO[2], EBO[2];
-    glGenVertexArrays(2, VAO);
-    glGenBuffers(2, VBO);
-    glGenBuffers(2, EBO);
+    glBindVertexArray(VAO);
 
-    glBindVertexArray(VAO[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // координатные артибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // цветовые атрибуты
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // текстурные атрибуты
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
- //------------------------------------
-    glBindVertexArray(VAO[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // координатные артибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    // координатные атрибуты
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // цветовые атрибуты
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    // атрибуты текстурных координат
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // текстурные атрибуты
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // загрузка и создание текстуры
     // -------------------------
-    stbi_set_flip_vertically_on_load(true);
-    unsigned int textures[2];
-    glGenTextures(2, textures);
-
-    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // все последующие GL_TEXTURE_2D-операции теперь будут влиять на данный текстурный объект
     // установка параметров наложения текстуры
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // установка параметров фильтрации текстуры
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // загрузка изображения, создание текстуры и генерирование mipmap-уровней
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\awesomeface.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    glBindTexture(GL_TEXTURE_2D, textures[1]); // все последующие GL_TEXTURE_2D-операции теперь будут влиять на данный текстурный объект
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	// установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    data = stbi_load("C:\\learnOpenGl-CLion\\src\\assets\\textures\\wooden_container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("C:\\learnOpenGl-VS\\src\\assets\\textures\\wooden_container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -165,47 +132,32 @@ int main(void)
     }
     stbi_image_free(data);
 
-    ourShader.use();
-    ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
-    float ratio = 0.5f;
-    ourShader.setFloat("ratio", ratio);
-
-
-    glm::mat4 identityMatrix = glm::mat4(1.0f);
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glm::mat4 trans;
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window, ourShader, ratio);
-        /* Render here */
+        processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Связывание текстуры
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, textures[0]);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, textures[1]);
+        glBindTexture(GL_TEXTURE_2D, texture);
 
-        float timeValue = glfwGetTime();
-        trans = glm::translate(identityMatrix, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, timeValue, glm::vec3(0.0f, 0.0f, 1.0f));
         ourShader.use();
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-        glBindVertexArray(VAO[0]);
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        modelLoc = glGetUniformLocation(ourShader.ID, "view");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-        glBindVertexArray(VAO[1]);
-        float scale = sin(timeValue); 
-        //std::cout << scale << std::endl;
-        trans = glm::translate(identityMatrix, glm::vec3(-0.5f, 0.5f, 0.0f));
-        trans = glm::scale(trans, glm::vec3(scale, scale, 0));
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &trans[0][0]);
-        ourShader.use();
+        modelLoc = glGetUniformLocation(ourShader.ID, "projection");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
@@ -214,10 +166,10 @@ int main(void)
         glfwPollEvents();
     }
     // Опционально: освобождаем все ресурсы, как только они выполнили своё предназначение
-    glDeleteVertexArrays(2, VAO);
-    glDeleteBuffers(2, VBO);
-    glDeleteBuffers(2, EBO);
-    glDeleteTextures(2, textures);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteTextures(1, &texture);
 
     glfwTerminate();
     return 0;
@@ -239,8 +191,15 @@ void processInput(GLFWwindow *window, RenderEngine::ShaderProgram &ourShader, fl
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
         changeRatioVisibleImage(ourShader, ratio, 0.001f);
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
         changeRatioVisibleImage(ourShader, ratio, -0.001f);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }

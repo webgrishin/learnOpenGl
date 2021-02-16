f693ac9f46b76206723a250c4662c7956a7280ab
Konstantin <webgrishin@gmail.com>
2021-02-16 19:56:03 +0300

    Рефакторинг кода

--- src/Renderer/Camera.h
+++ src/Renderer/Camera.h
@@ -46,28 +46,121 @@ namespace RenderEngine
 		// typedef void(Camera::* Pf)(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 		// Конструктор, использующий векторы
-		Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
-
+		Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+		{
+			Position = position;
+			WorldUp = up;
+			Yaw = yaw;
+			Pitch = pitch;
+			updateCameraVectors();
+		}
 		// Конструктор, использующие скаляры
-		Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+		Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+		{
+			Position = glm::vec3(posX, posY, posZ);
+			WorldUp = glm::vec3(upX, upY, upZ);
+			Yaw = yaw;
+			Pitch = pitch;
+			updateCameraVectors();
+		}
 
 		// Возвращает матрицу вида, вычисленную с использованием углов Эйлера и LookAt-матрицы
-		glm::mat4 GetViewMatrix();
+		glm::mat4 GetViewMatrix()
+		{
+			//return myLookAt();
+			return glm::lookAt(Position, Position + Front, Up);
+		}
 
 		//Обрабатываем входные данные, полученные от любой клавиатуроподобной системы ввода. Принимаем входной параметр в виде определенного камерой перечисления (для абстрагирования его от оконных систем)
-		void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+		void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+		{
+			float velocity = MovementSpeed * deltaTime;
+			//float velocity = 1;
+			if (direction == FORWARD)
+				Position += Front * velocity;
+			if (direction == BACKWARD)
+				Position -= Front * velocity;
+			if (direction == LEFT)
+				Position -= Right * velocity;
+			if (direction == RIGHT)
+				Position += Right * velocity;
+
+			//Position.y = 0.0f; //Оставться на земле, в плоскости xz
+		}
 
 		//Обрабатываем входные данные, полученные от системы ввода с помощью мыши. Ожидаем в качестве параметров значения смещения как в направлении X, так и в направлении Y.
-		void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+		void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+		{
+			xoffset *= MouseSensitivity;
+			yoffset *= MouseSensitivity;
+
+			Yaw += xoffset;
+			Pitch += yoffset;
+
+			// Убеждаемся, что когда тангаж выходит за пределы обзора, экран не переворачивается
+			if (constrainPitch)
+			{
+				if (Pitch > 89.0f)
+					Pitch = 89.0f;
+				if (Pitch < -89.0f)
+					Pitch = -89.0f;
+			}
+
+			// Обновляем значения вектора-прямо, вектора-вправо и вектора-вверх, используя обновленные значения углов Эйлера
+			updateCameraVectors();
+		}
 
 		// Обрабатывает входные данные, полученные от события колеса прокрутки мыши. Интересуют только входные данные на вертикальную ось колесика
-		void ProcessMouseScroll(float yoffset);
+		void ProcessMouseScroll(float yoffset)
+		{
+			if (Zoom >= 1.0f && Zoom <= 45.0f)
+				Zoom -= yoffset;
+			if (Zoom <= 1.0f)
+				Zoom = 1.0f;
+			if (Zoom >= 45.0f)
+				Zoom = 45.0f;
+		}
 
 	private:
 		// Вычисляет вектор-прямо по (обновленным) углам Эйлера камеры
-		void updateCameraVectors();
-		glm::mat4 myLookAt();
+		void updateCameraVectors()
+		{
+			// Вычисляем новый вектор-прямо
+			glm::vec3 front;
+			front.x = cos(glm::radians(Yaw)); // *cos(glm::radians(Pitch));
+			front.y = sin(glm::radians(Pitch));
+			front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+			Front = glm::normalize(front);
+			// Также пересчитываем вектор-вправо и вектор-вверх
+			Right = glm::normalize(glm::cross(Front, WorldUp)); // Нормализуем векторы, потому что их длина становится стремится к 0 тем больше, чем больше вы смотрите вверх или вниз, что приводит к более медленному движению.
+			Up = glm::normalize(glm::cross(Right, Front));
+		}
+
+		glm::mat4 myLookAt()
+		{
+			//glm::vec3 target = Position + Front;
+			//glm::vec3 front = glm::normalize(Position - target);
+			//glm::vec3 right = glm::normalize(glm::cross(WorldUp, front));
+			//glm::vec3 up = glm::cross(front, right);
+
+			glm::mat4 lookAt(1.0f);
+			lookAt[0][0] = Right.x;
+			lookAt[1][0] = Right.y;
+			lookAt[2][0] = Right.z;
+			lookAt[0][1] = Up.x;
+			lookAt[1][1] = Up.y;
+			lookAt[2][1] = Up.z;
+			lookAt[0][2] = -Front.x;
+			lookAt[1][2] = -Front.y;
+			lookAt[2][2] = -Front.z;
+
+			glm::mat4 position(1.0f);
+			position[3][0] = -Position.x;
+			position[3][1] = -Position.y;
+			position[3][2] = -Position.z;
+			return lookAt * position;
+		}
 	};
-} // namespace RenderEngin
+} // namespace RenderEngine
 
 #endif //LEARNOPENGL_CAMERA_H
--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -13,41 +13,168 @@
 #include <iostream>
 #include <glm/gtc/type_ptr.hpp>
 
-namespace RenderEngine {
-    class ShaderProgram {
+namespace RenderEngine
+{
+    class ShaderProgram
+    {
     public:
         unsigned int ID;
 
         // конструктор генерирует шейдер на лету
+        ShaderProgram(const std::string &vertexPath, const std::string &fragmentPath)
+        {
+            // 1. получение исходного кода вершинного/фрагментного шейдера из переменной filePath
+            std::string vertexCode;
+            std::string fragmentCode;
+            std::ifstream vShaderFile;
+            std::ifstream fShaderFile;
+            // убеждаемся, что объекты ifstream могут выбросить исключение:
+            vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            try
+            {
+                // открываем файлы
+                vShaderFile.open(vertexPath.c_str());
+                fShaderFile.open(fragmentPath.c_str());
+                std::stringstream vShaderStream, fShaderStream;
+                // читаем содержимое файловых буферов
+                vShaderStream << vShaderFile.rdbuf();
+                fShaderStream << fShaderFile.rdbuf();
+                // закрываем файлы
+                vShaderFile.close();
+                fShaderFile.close();
+                // конвертируем в строковую переменную данные из потока
+                vertexCode = vShaderStream.str();
+                fragmentCode = fShaderStream.str();
+            }
+            catch (std::ifstream::failure &e)
+            {
+                std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << "-" << e.code() << std::endl;
+                m_isCompiled = false;
+                return;
+            }
+            // 2. компилируем шейдеры
+            GLuint idVertex, idFragment;
+            // vertex shader
+            idVertex = createShader(GL_VERTEX_SHADER, vertexCode.c_str());
+            if (!m_isCompiled)
+            {
+                glDeleteShader(idVertex);
+                return;
+            }
+
+            // Фрагментный шейдер
+            idFragment = createShader(GL_FRAGMENT_SHADER, fragmentCode.c_str());
+            if (!m_isCompiled)
+            {
+                glDeleteShader(idFragment);
+                return;
+            }
+
+            // Шейдерная программа
+            ID = glCreateProgram();
+            glAttachShader(ID, idVertex);
+            glAttachShader(ID, idFragment);
+            glLinkProgram(ID);
+            checkCompileErrors(ID, "PROGRAM");
+            // После того, как мы связали шейдеры с нашей программой, удаляем их, т.к. они больше не нужны
+            glDeleteShader(idVertex);
+            glDeleteShader(idFragment);
+        }
+
+        ~ShaderProgram()
+        {
+            glDeleteProgram(ID);
+        }
+
+        // активация шейдера
+        void use() const
+        {
+            glUseProgram(ID);
+        }
+
+        bool isCompiled() const
+        {
+            return m_isCompiled;
+        }
+
+        // полезные униформ-функции
+        void setMat4(const std::string &name, glm::mat4 &value) const
+        {
+            //glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+            glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &value[0][0]);
+        }
+
+        void setVec3(const std::string &name, glm::vec3 &value) const
+        {
+            glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+        }
+        void setVec3(const std::string &name, float x, float y, float z) const
+        {
+            glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+        }
+
+        void setBool(const std::string &name, bool value) const
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+        }
+
+        void setInt(const std::string &name, int value) const
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+        }
+
         // ------------------------------------------------------------------------
-        ShaderProgram(const std::string& vertexPath, const std::string& fragmentPath);
-
-        ~ShaderProgram();
-
-        bool isCompiled() const { return m_isCompiled; }
-
-        void use() const;
-        
-        void setMat4(const std::string& name, glm::mat4& value) const;
-        
-        void setVec3(const std::string& name, glm::vec3& value) const;
-        void setVec3(const std::string& name, float x, float y, float z) const;
-
-        void setBool(const std::string &name, bool value) const;
-
-        void setInt(const std::string &name, int value) const;
-
-        void setFloat(const std::string &name, float value) const;
+        void setFloat(const std::string &name, float value) const
+        {
+            glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+        }
 
     private:
+        bool m_isCompiled = true;
+        // полезные функции для проверки ошибок компиляции/связывания шейдеров.
+        GLuint createShader(GLenum shaderType, const char *sourceCode)
+        {
+            GLuint idShader = glCreateShader(shaderType);
+            glShaderSource(idShader, 1, &sourceCode, nullptr);
+            glCompileShader(idShader);
+            checkCompileErrors(idShader, shaderType == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT");
+
+            return idShader;
+        }
+
         // полезные функции для проверки ошибок компиляции/связывания шейдеров.
         // ------------------------------------------------------------------------
-        void checkCompileErrors(unsigned int shader, std::string type);
-
-        GLuint createShader(GLenum shaderType, const char *sourceCode);
-
-        bool m_isCompiled = true;
+        void checkCompileErrors(unsigned int shader, std::string type)
+        {
+            int success;
+            char infoLog[1024];
+            if (type != "PROGRAM")
+            {
+                glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+                if (!success)
+                {
+                    m_isCompiled = false;
+                    glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                    std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+                              << infoLog
+                              << "\n -- --------------------------------------------------- -- " << std::endl;
+                }
+            }
+            else
+            {
+                glGetProgramiv(shader, GL_LINK_STATUS, &success);
+                if (!success)
+                {
+                    m_isCompiled = false;
+                    glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                    std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                              << infoLog
+                              << "\n -- --------------------------------------------------- -- " << std::endl;
+                }
+            }
+        }
     };
-}
+} // namespace RenderEngine
 
 #endif //LEARNOPENGL_SHADERPROGRAM_H

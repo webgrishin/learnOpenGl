7b25dc2c922efc0ac838ef108f263101e6b9a7cb
Konstantin <webgrishin@gmail.com>
2021-04-15 16:45:21 +0300

    Внёс фиксы, проекция пламени стала более правильной

--- src/Renderer/Fire.cpp
+++ src/Renderer/Fire.cpp
@@ -5,21 +5,23 @@ Fire::Fire(vec2 position, GLfloat width, GLuint wWdth, GLuint wHeight)
     this->shader.create("assets/shaders/point.vs", "assets/shaders/point.fs");
     // GLfloat onePxPart = 2.0f / (GLfloat)wWdth; //шаг смещения (количество частей в одном пикселе)
     vec2 nextPosition = position;
-    GLint inversion, j;
+    GLint inversion, i;
     // GLfloat interval; 
     GLfloat scaleX, scaleY, length;
     // GLuint k = 0;
     srand(static_cast<unsigned int>(time(0)));
     rand();
-    GLfloat m, n, k, l, step, c;
-    c = 100.0f; //100 язычков пламени
+    GLfloat m, n, k, l, step, x, nx;
+    GLfloat const c = 100.0f; //100 язычков пламени
 
-    k = 2.0f;
+    k = 3.0f;
     m = M_PI/(2*k);
     n = -m;
     l = m*2;
     step = l/c;
     scaleY = 1.0f/l;
+
+    GLfloat xs[(uint)(c-1)];
 
     GLfloat offset = width/c;
     // onePxPart *= offset;
@@ -29,10 +31,48 @@ Fire::Fire(vec2 position, GLfloat width, GLuint wWdth, GLuint wHeight)
     GLfloat y, y0, dy, yMax, kl;
     // kl = 2.0f;
     kl = width * scaleY;
-    j = 0;
+    i = 0;
     y0 = 0;
-    GLfloat i=position.x;
+    // GLfloat i=position.x;
     printf("n: %f, l: %f, step: %f; offset:%f\n", n, l, step, offset);
+
+    nx= n;
+    for (x = n+step; x < m-step; x += step, i++){
+        y = cos(x*k);
+        dy = y - y0;
+        y0 = y;
+        xs[i] = nx + abs(dy) / 2.0f * l;
+        nx = xs[i];
+        printf("i: %i, y: %f\n", i, xs[i]);
+        // i++;
+    }
+
+    GLfloat kx = width / l;
+
+    for (i=0; i < sizeof(xs)/sizeof(xs[0]); i++){
+        x = xs[i];
+        y = cos(x*k);
+        yMax = y*kl;
+        length = getRandomNumber((int)(0.6 * yMax * 1000.0f), (int)(yMax * 1000.0f)) / 1000.0f;
+        // length = yMax;
+
+        if (length > 0.4f)
+        {
+            nextPosition.x = kx * x;
+
+            inversion = getRandomNumber(0, 1);
+            inversion = inversion == 0 ? -1 : 1;
+            // interval = getRandomNumber(10, 20) / 1000.0f; //0.02f;
+            scaleX = getRandomNumber(10, 50) / 10.0f;
+            // scaleY = 1.0f;
+            scaleY = getRandomNumber(5, 30) / 100.0f;
+            BrushGenerator brush = BrushGenerator(this->shader, nextPosition, length, scaleX, scaleY, inversion);
+            this->brushes.push_back(brush);
+        }
+    }
+
+    return;
+
     for (GLfloat x = n+step; x < m; x += step){
         y = cos(x*k);
         // y = abs(x*k)*-1.0f+1.0f;
@@ -42,7 +82,7 @@ Fire::Fire(vec2 position, GLfloat width, GLuint wWdth, GLuint wHeight)
 
         // i += offset;
         // nextPosition.x = i;
-        if (length > 0.0f)
+        if (length > 0.4f)
         {
             {
                 dy = y - y0;
@@ -50,7 +90,7 @@ Fire::Fire(vec2 position, GLfloat width, GLuint wWdth, GLuint wHeight)
                 position.x += abs(dy)/2.0f * width;
                 nextPosition.x = position.x;
             }
-            printf("i:%i; y: %f; yMax: %f; x:%f; ody:%f; dx:%f; xn:%f; l: %f\n", j++, y, yMax, nextPosition.x, dy, dy*width, position.x, length);
+            printf("i:%i; y: %f; yMax: %f; x:%f; ody:%f; dx:%f; xn:%f; l: %f\n", i++, y, yMax, nextPosition.x, dy, dy*width, position.x, length);
             inversion = getRandomNumber(0, 1);
             inversion = inversion == 0 ? -1 : 1;
             // interval = getRandomNumber(10, 20) / 1000.0f; //0.02f;
